69a8ef3f50ce34be63c043a4e458751d4fdd95cc
Hank Shen <swh@admpub.com>
2022-03-12 23:36:56 +0800

    update

---
+++ group.go
@@ -0,0 +1,110 @@
+package lock
+
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+// Group ...
+type Group interface {
+	Lock(key interface{})
+	TryLock(key interface{}) bool
+	TryLockWithTimeout(key interface{}, duration time.Duration) bool
+	TryLockWithContext(key interface{}, ctx context.Context) bool
+	Unlock(key interface{})
+	UnlockAndFree(key interface{})
+}
+
+func NewGroup(fn func() Mutex) Group {
+	if fn == nil {
+		fn = func() Mutex {
+			return NewChanMutex()
+		}
+	}
+	return &group{
+		fn:    fn,
+		group: make(map[interface{}]*entry),
+	}
+}
+
+type group struct {
+	mu    sync.RWMutex
+	fn    func() Mutex
+	group map[interface{}]*entry
+}
+
+type entry struct {
+	ref int32
+	mu  Mutex
+}
+
+func (m *group) get(i interface{}, ref int32) Mutex {
+	m.mu.RLock()
+	en, ok := m.group[i]
+	m.mu.RUnlock()
+	if !ok {
+		if ref > 0 {
+			en = &entry{mu: m.fn()}
+			m.mu.Lock()
+			m.group[i] = en
+			m.mu.Unlock()
+		} else {
+			return nil
+		}
+	}
+	atomic.AddInt32(&en.ref, ref)
+	return en.mu
+}
+
+func (m *group) Lock(i interface{}) {
+	m.get(i, 1).Lock()
+}
+
+func (m *group) Unlock(i interface{}) {
+	mu := m.get(i, -1)
+	if mu != nil {
+		mu.Unlock()
+	}
+}
+
+func (m *group) UnlockAndFree(i interface{}) {
+	m.mu.RLock()
+	en, ok := m.group[i]
+	m.mu.RUnlock()
+	if !ok {
+		return
+	}
+	atomic.AddInt32(&en.ref, -1)
+	if atomic.LoadInt32(&en.ref) == 0 {
+		m.mu.Lock()
+		delete(m.group, i)
+		m.mu.Unlock()
+	}
+	en.mu.Unlock()
+}
+
+func (m *group) TryLock(i interface{}) bool {
+	locked := m.get(i, 1).TryLock()
+	if !locked {
+		m.get(i, -1)
+	}
+	return locked
+}
+
+func (m *group) TryLockWithTimeout(i interface{}, timeout time.Duration) bool {
+	locked := m.get(i, 1).TryLockWithTimeout(timeout)
+	if !locked {
+		m.get(i, -1)
+	}
+	return locked
+}
+
+func (m *group) TryLockWithContext(i interface{}, ctx context.Context) bool {
+	locked := m.get(i, 1).TryLockWithContext(ctx)
+	if !locked {
+		m.get(i, -1)
+	}
+	return locked
+}
---
+++ group_test.go
@@ -0,0 +1,149 @@
+package lock
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestGroup(t *testing.T) {
+	mu := NewGroup(nil)
+	mu.Lock("g")
+	defer mu.Unlock("g")
+	if mu.TryLock("g") {
+		t.Errorf("cannot fetch mutex !!!")
+	}
+}
+
+func TestGroupMutliWaitLock(t *testing.T) {
+	var (
+		wg sync.WaitGroup
+		mu = NewGroup(nil)
+		cn = 3
+	)
+
+	for i := 0; i < cn; i++ {
+		wg.Add(1)
+		go func() {
+			mu.Lock("h")
+			time.Sleep(1e7)
+			mu.Unlock("h")
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	for i := 0; i < cn; i++ {
+		wg.Add(1)
+		go func() {
+			mu.Lock("g")
+			time.Sleep(1e7)
+			mu.UnlockAndFree("g")
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+func TestGroupUnlockAndFree(t *testing.T) {
+	var (
+		wg sync.WaitGroup
+		mu = NewGroup(nil)
+		mg = mu.(*group)
+	)
+
+	for j := 1; j < 5; j++ {
+		for i := 0; i < j; i++ {
+			wg.Add(1)
+			go func() {
+				mu.Lock("h")
+				time.Sleep(1e6)
+				mu.UnlockAndFree("h")
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+		mg.mu.Lock()
+		if _, ok := mg.group["h"]; ok {
+			t.Error("h mutex exist after UnlockAndFree")
+		}
+		mg.mu.Unlock()
+	}
+}
+
+func TestGroupTryLockFailedAndUnlockAndFree(t *testing.T) {
+	var (
+		wg sync.WaitGroup
+		mu = NewGroup(nil)
+		mg = mu.(*group)
+	)
+
+	for j := 1; j < 5; j++ {
+		for i := 0; i < j; i++ {
+			wg.Add(1)
+			go func() {
+				if mu.TryLock("h") {
+					time.Sleep(1e6)
+					mu.UnlockAndFree("h")
+				}
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+		mg.mu.Lock()
+		if _, ok := mg.group["h"]; ok {
+			t.Error("h mutex exist after UnlockAndFree")
+		}
+		mg.mu.Unlock()
+	}
+}
+
+func TestGroupTryLockTimeout(t *testing.T) {
+	mu := NewGroup(nil)
+	mu.Lock("g")
+	go func() {
+		time.Sleep(1 * time.Millisecond)
+		mu.Unlock("g")
+	}()
+	if mu.TryLockWithTimeout("g", 500*time.Microsecond) {
+		t.Errorf("cannot fetch mutex in 500us !!!")
+	}
+	if !mu.TryLockWithTimeout("g", 5*time.Millisecond) {
+		t.Errorf("should fetch mutex in 5ms !!!")
+	}
+	mu.Unlock("g")
+}
+
+func TestGroupTryLockContext(t *testing.T) {
+	mu := NewGroup(nil)
+	ctx, cancel := context.WithCancel(context.Background())
+	mu.Lock("g")
+	go func() {
+		time.Sleep(10 * time.Millisecond)
+		cancel()
+	}()
+	if mu.TryLockWithContext("g", ctx) {
+		t.Errorf("cannot fetch mutex !!!")
+	}
+}
+
+func BenchmarkGroup(b *testing.B) {
+	mu := NewGroup(nil)
+	a := 0
+	c := 0
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			mu.Lock("g")
+			a++
+			mu.Unlock("g")
+			mu.Lock("g")
+			c = a
+			mu.Unlock("g")
+		}
+	})
+	_ = a
+	_ = c
+}
